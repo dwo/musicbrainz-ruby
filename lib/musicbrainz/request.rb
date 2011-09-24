5697a00f363bed4c32e40b9561b261659a912fa9
Robin Tweedie <phorque@gmail.com>
2011-09-24 16:41:16 +0100

    refactoring request logic out of client

---
+++ lib/musicbrainz/request.rb
@@ -0,0 +1,28 @@
+module MusicBrainz
+  class Request
+    UNIQUE_IDENTIFIERS = %q{discid puid isrc iswc}
+
+    def initialize(resource, params)
+      @resource = resource
+      @params   = params
+      @id       = nil
+
+      if UNIQUE_IDENTIFIERS.include?(resource)
+        @id = params.delete(resource.to_sym)
+      elsif params.has_key?(:mbid)
+        @id = params.delete(:mbid)
+      end
+    end
+
+    def path
+      path = "/#{@resource}/#{@id}"
+    end
+
+    def options
+      options = {}
+      options[:query] = @params unless @params.empty?
+      options
+    end
+
+  end
+end
