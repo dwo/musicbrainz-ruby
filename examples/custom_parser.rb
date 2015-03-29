09581bbd7ab11871a6f9a875a5adf1fcb2875f44
Robin Tweedie <robin@songkick.com>
2015-03-29 13:34:57 +0100

    working example for v0 style parsing

--- examples/custom_parser.rb
+++ examples/custom_parser.rb
@@ -4,14 +4,16 @@ require 'hashie/mash'
 class MusicBrainz::Client
   class ParseMash < HTTParty::Parser
     def parse
-      parsed = MultiJson.parse(body)
-      Hashie::Mash.new(parsed['metadata'])
+      parsed = MultiXml.parse(body)
+      Hashie::Mash.new(parsed)
     end
   end
 
   parser ParseMash
 end
 
-client = Musicbrainz::Client.new
-client.artist(:query => 'Diplo')
-# =>
+client = MusicBrainz::Client.new
+response = client.artist(:query => 'Diplo')
+
+response.artist_list.artist.first.id
+# => "a56bd8f9-8ef8-4d63-89a4-794ed1360dd2"
